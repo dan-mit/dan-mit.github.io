87d814b5a16714437c2e781cb9f29935eab64fa2
Daniil <daniilmitev@gmail.com>
2023-09-24 19:10:49 -0400

    Added and edit button feature as well as an inline editor for the edits. App will now change size based on content, selecting the task instead of the checkbox no longer marks a task as complete

--- script.js
+++ script.js
@@ -1,33 +1,74 @@
 //References to the HTML List and Input elements
-const inputBox = document.getElementById("inputBox")
-const taskList = document.getElementById("taskList")
+const inputBox = document.getElementById("inputBox");
+const taskList = document.getElementById("taskList");
+const appContainer = document.querySelector('.app');
 //Handles the event of any empty task being added
 function addTask(){
     if(inputBox.value == ''){
         alert("Task cannot be empty.");
+        return;
     }
-    else{
+
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        taskList.appendChild(li);
+        //creates a span to allow for editing
+        let taskSpan = document.createElement("span");
+        taskSpan.classList.add("task");
+        taskSpan.innerHTML = inputBox.value;
+        li.appendChild(taskSpan);
+        //the following allows for an edit button with edit functionality
+        let editBtn = document.createElement("button");
+        editBtn.innerHTML = "Edit";
+        editBtn.classList.add("edit-button");
+        li.appendChild(editBtn);
         //the following allows for an x to be placed at the end of the task
         let cross = document.createElement("cross");
         cross.innerHTML = "\u00d7";
-        li.appendChild(cross)
-    }
+        li.appendChild(cross);
+    
+        taskList.appendChild(li);
     inputBox.value = "";
     saveData();
 }
 /* The following checks where on the task row the user has clicked
 and either checks the task off or removes if clicked on cross */
-taskList.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+taskList.addEventListener("click", function(n){
+    if(n.target.tagName === "LI" && !n.target.hasAttribute('data-editing')){
+        n.target.classList.toggle("checked");
         saveData();
     }
-    else if(e.target.tagName === "CROSS"){
-        e.target.parentElement.remove();
+    else if(n.target.tagName === "CROSS"){
+        n.target.parentElement.remove();
         saveData();
+    }
+    /*The following code allows for the edit button to work in-line rather
+    rather than prmpting in the browser*/
+    else if(n.target.innerHTML === "Edit"){
+        let taskSpan = n.target.previousSibling;
+        let currentTask = taskSpan.innerText;
+        //The code works by replacing the span element of the task with an editable text
+        let inputField = document.createElement("input");
+        inputField.type = "text";
+        inputField.value = currentTask;
+        let spanWidth = taskSpan.offsetWidth;
+        inputField.style.width = (spanWidth - 100) + "px"
+        taskSpan.parentElement.replaceChild(inputField, taskSpan);
+
+        inputField.focus();
+        inputField.select();
+        /*This event listener will wait for the enter key being pressed or for 
+        the textbox to lose focus in order to save the textbox back to a span*/    
+        inputField.addEventListener("blur", saveInlineEdit);
+        inputField.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                saveInlineEdit.call(inputField); // Use call to ensure 'this' refers to inputField
+            }
+        n.target.parentElement.setAttribute('data-editing','true');
+        });
+        //This is the old way of editing the task, by prompting
+        /* let newTask = prompt("Edit Task: ", taskSpan.innerHTML)
+        if(newTask) taskSpan.innerHTML = newTask;
+        saveData(); */
     }
 }, false);
 //The following listener checks for the enter ket to be pressed to append task to list
@@ -44,6 +85,18 @@ function saveData(){
 }
 function showList(){
     taskList.innerHTML = localStorage.getItem("data");
+}
+//This function takes the text box of the edited element and reconverts the text back to a span
+function saveInlineEdit(){
+    let newText = this.value;
+    let newTaskSpan = document.createElement("span");
+
+    newTaskSpan.classList.add("task");
+    newTaskSpan.innerText = newText;
+    this.parentElement.replaceChild(newTaskSpan, this);
+    this.parentElement.removeAttribute('data-editing');
+
+    saveData();
 }
 
 showList();
